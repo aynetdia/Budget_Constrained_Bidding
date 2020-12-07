87aa0f6633a7a83697b3b2ab6cffc2240e4397e5
Ansar Aynetdinov <aynetdia@hu-berlin.de>
2020-12-07 21:03:29 +0100

    Greedy policy, model and memory checkpointing, history saving, fixed the bid function, adjustments to the act function (w.r.t. ctr, min and inference as well)

--- src/rtb_agent/rl_bid_agent.py
+++ src/rtb_agent/rl_bid_agent.py
@@ -2,6 +2,8 @@ import sys,os
 sys.path.append(os.getcwd()+'/src/gym-auction_emulator')
 import gym, gym_auction_emulator
 import random
+from operator import itemgetter
+import pickle
 import torch
 import numpy as np
 from collections import deque
@@ -10,6 +12,7 @@ import configparser
 from dqn import Agent
 from reward_net import RewardNet
 import numpy as np
+import pandas as pd
 
 
 class RlBidAgent():
@@ -32,15 +35,17 @@ class RlBidAgent():
         # Control parameter used to scale bid price
         self.BETA = [-0.08, -0.03, -0.01, 0, 0.01, 0.03, 0.08]
         self.eps = 0.9
-        self.anneal = 1e-5
+        self.anneal = 2e-5
         self.t_step = 0
-        self.budget_spend = 0.0
+        self.global_T = 0
+        self.episode_budgets = None
+        self.budget_spend_t = 0.0
         self.reward_t = 0
-        self.ROL = self.T # 3. the number of Lambda regulation opportunities left
-        self._reset_episode()
+        self.bid_memory = []
+        self.step_memory = []
         # DQN Network to learn Q function
         self.dqn_agent = Agent(state_size = self.STATE_SIZE, action_size=self.ACTION_SIZE, seed =0)
-        # Reward Network to reward function
+        # Reward Network to learn the reward function
         self.reward_net = RewardNet(state_action_size = self.STATE_SIZE + 1, reward_size=1, seed =0)
         self.dqn_state = None
         self.dqn_action = 3 # first action - no adjustment
@@ -53,9 +58,13 @@ class RlBidAgent():
         Function to reset the state when episode changes
         """
         self._reset_step()
+        self.greedy_memory = []
+        self.budget = self.episode_budgets.pop(0)
+        self.rem_budget = self.budget
+        self.ROL = self.T # 3. the number of Lambda regulation opportunities left
         self.cur_day = 0
-        self.cur_hour = 0
-        self.ctl_lambda = 1e-3  # Lambda sequential regulation parameter
+        self.cur_min = 0
+        self.ctl_lambda = self.calc_greedy(self.greedy_memory, self.budget)  # Lambda sequential regulation parameter
         self.wins_e = 0  
         self.reward_net.V = 0
         self.reward_net.S = []
@@ -66,11 +75,11 @@ class RlBidAgent():
         """
         self.t_step += 1
         self.prev_budget = self.rem_budget # Bt-1
-        self.rem_budget = self.budget - self.budget_spend # Bt (2. the remaining budget at time-step t)
+        self.rem_budget = self.budget - self.budget_spend_t # Bt (2. the remaining budget at time-step t)
         self.ROL -= 1
         self.BCR = (self.rem_budget - self.prev_budget) / self.prev_budget
         self.CPM = self.cost_t / (self.wins_t/1000) # cost of the won ads divided by the number of impressions (expressed in thousands)
-        self.WR = self.total_wins / self.imp_opps 
+        self.WR = self.wins_t / self.imp_opps 
 
     def _reset_step(self):
         """
@@ -81,6 +90,7 @@ class RlBidAgent():
         self.cost_t = 0.
         self.wins_t = 0
         self.bids_t = 0
+        self.budget_spend_t = 0
         self.imp_opps = 0
         self.BCR = 0                      # 4. Budget consumption rate
         self.CPM = 0                      # 5. Cost per mille of impressions between t-1 and t:
@@ -112,36 +122,44 @@ class RlBidAgent():
                 self.WR,
                 self.rewards_prev_t])
 
-    def act(self, state, reward, cost):
+    def act(self, state, reward, cost, eval_flag=False):
         """
         This function gets called with every bid request.
         By looking at the weekday and hour to progress between the steps and
         episodes during training.
-        Returns the bid request cost based on the scaled version of the
+        Returns the bid decision based on the scaled version of the
         bid price using the DQN agent output.
         """
-        episode_done = (state['weekday'] != self.cur_day)
         # within the time step
-        if state['hour'] == self.cur_hour and state['weekday'] == self.cur_day:
+        if state['min'] == self.cur_min and state['weekday'] == self.cur_day:
             pass
         # within the episode, changing the time step
-        elif state['hour'] != self.cur_hour and state['weekday'] == self.cur_day:
+        elif state['min'] != self.cur_min and state['weekday'] == self.cur_day:
             self._update_step()
-            self.reward_net.step() # update reward net
+            # self.reward_net.step() # update reward net
             dqn_next_state = self._get_state() # observe state s_1 (state at the beginning of t=1)
-            # get action a_1 (adjusting lambda_0 to lambda_1) from the adaptive greedy policy
-            a_beta = self.dqn_agent.act(dqn_next_state, eps=self.eps)
-            self.ctl_lambda *= (1 + self.BETA[a_beta]) # adjust lambda t-1 to t (0 to 1)
-            sa = np.append(self.dqn_state, self.dqn_action) # state-action pair for t=0
-            rnet_r = float(self.reward_net.act(sa)) # get reward r_0 from RewardNet
-            self.reward_net.V += self.reward_t
-            self.reward_net.S.append((self.dqn_state, self.dqn_action))
-            # call agent step
-            # Sample a mini batch and perform grad-descent step
-            self.dqn_agent.step(self.dqn_state, self.dqn_action, rnet_r, dqn_next_state, episode_done)
+
+            if self.rem_budget <= cost:
+                pass
+            else:
+                # get action a_1 (adjusting lambda_0 to lambda_1) from the adaptive greedy policy
+                self.reward_net.step(eval_flag) # update reward net
+                a_beta = self.dqn_agent.act(dqn_next_state, eps=self.eps)
+                self.ctl_lambda *= (1 + self.BETA[a_beta]) # adjust lambda t-1 to t (0 to 1)
+                sa = np.append(self.dqn_state, self.dqn_action) # state-action pair for t=0
+                rnet_r = float(self.reward_net.act(sa)) # get reward r_0 from RewardNet
+                self.reward_net.V += self.reward_t
+                self.reward_net.S.append((self.dqn_state, self.dqn_action))
+                # call agent step
+                # Sample a mini batch and perform grad-descent step
+                self.dqn_agent.step(self.dqn_state, self.dqn_action, rnet_r, dqn_next_state, eval_flag)
+                self.dqn_action = a_beta # analogously with the action t+1
+
             self.dqn_state = dqn_next_state # set state t+1 as state t (in order call it during next transition)
-            self.dqn_action = a_beta # analogously with the action t+1
-            self.cur_hour = state['hour'] 
+            self.cur_min = state['min'] 
+            self.global_T += 1
+            # save state, action, lambda, epsilon, rewardnet history here
+            self.step_memory.append(np.append([self.global_T, self._get_state, self.ctl_lambda, self.eps, a_beta, self.dqn_agent.loss, rnet_r, self.reward_net.loss]).tolist())
             self._reset_step()
         # episode changes
         elif state['weekday'] != self.cur_day:
@@ -151,24 +169,53 @@ class RlBidAgent():
                 self.reward_net.add_to_M(sa, max_r)
                 self.reward_net.add(sa, max_r)
             print("Total Impressions won with Budget={} Spend={} wins = {}".format(self.budget, self.budget_spend, self.wins_e))
-            self.total_wins += self.wins_e
             self._reset_episode() 
             self.cur_day = state['weekday']
-            self.cur_hour = state['hour']
+            self.cur_min = state['min']
 
         self.imp_opps += 1
-        bid = self.bid(state['click'], reward, cost) # bid with lambda_0
+        bid = self.bid(state['pCTR'], reward, cost) # bid with lambda_0
+        self.greedy_memory.append([state['pCTR'], state['payprice']])
+
+        # save bid history here
+        self.bid_memory.append([state['bidid'], bid, state['pCTR'], state['payprice'], state['click']])
 
         return bid
 
     def bid(self, imp_value, reward, cost):
-        bid_amt = imp_value/self.ctl_lambda
+        
+        bid_amt = min(self.rem_budget, round(imp_value/self.ctl_lambda, 2))
+
         if bid_amt > cost:
-            self.budget_spend += cost
-            self._update_reward_cost(reward, cost)
-        if bid_amt > 0
             self.bids_t += 1
+            self.budget_spend_t += cost
+            self._update_reward_cost(reward, cost)
+        elif bid_amt > 0:
+            self.bids_t += 1
+            self.budget_spend_t += bid_amt
+
         return bid_amt
+
+    def calc_greedy(self, items, budget_limit):
+        # Borrowed from: https://bitbucket.org/trebsirk/algorithms/src/master/knapsack.py
+        if len(items) == 0:
+            return 0.0001
+        else:
+            bids = []
+            spending = 0
+            ctr = 0
+            items_sorted = sorted(items, key=itemgetter(1))
+            while len(items_sorted) > 0:
+                item = items_sorted.pop()
+                if item[0] + spending <= budget_limit:
+                    bids.append(item)
+                    spending += bids[-1][1]
+                    ctr += bids[-1][0]
+                else:
+                    break
+            opt_lambda = np.mean(np.divide(ctr, spending))
+            return opt_lambda
+
 
 def main():
     # Instantiate the Environment and Agent
@@ -177,17 +224,47 @@ def main():
     agent = RlBidAgent()
 
     obs, reward, cost, done = env.reset()
+    train_budget = env.bid_requests.payprice.sum()/2
+    budget_proportions = []
+    for episode in env.bid_requests.weekday.unique():
+        budget_proportions.append(len(env.bid_requests[env.bid_requests.weekday == episode])/env.total_bids)
+    for i in range(len(budget_proportions)):
+        budget_proportions[i] = round(train_budget * budget_proportions[i])
+
+    agent.episode_budgets = budget_proportions
+    agent._reset_episode()
     agent.cur_day = obs['weekday']
     agent.cur_hour = obs['hour']
     agent.dqn_state = agent._get_state() # observe state s_0
 
-    while not done:
-        # action = bid amount
-        bid = agent.act(obs, reward, cost) # obs = state
-        next_obs, next_reward, next_cost, done = env.step(bid)
-        obs, reward, cost = next_obs, next_reward, next_cost # Next state assigned to current state
-        if cost > (agent.budget - agent.total_cost):
-            done == True
+    epochs = 450
+
+    for i in range(epochs):
+        while not done:
+            bid = agent.act(obs, reward, cost) # obs = state
+            next_obs, next_reward, next_cost, done = env.step(bid)
+            obs, reward, cost = next_obs, next_reward, next_cost # Next state assigned to current state
+        
+        if ((i + 1) % 25) == 0:
+
+            PATH_1 = sys.path.append(os.getcwd() + '/models/model_state_{}.tar'.format(epoch))
+            torch.save({'local_q_model': agent.dqn_agent.qnetwork_local.state_dict(),
+                        'target_q_model':agent.dqn_agent.qnetwork_target.state_dict(),
+                        'q_optimizer':agent.dqn_agent.optimizer.state_dict(),
+                        'rnet': agent.reward_net.reward_net.state_dict(),
+                        'rnet_optimizer': agent.reward_net.optimizer.state_dict()}, PATH_1)
+
+            PATH_2 = sys.path.append(os.getcwd() + '/models/replay_memories_{}'.format(epoch))
+            f = open(PATH_2, "w")
+            pickle.dump(agent.dqn_agent.memory)
+            pickle.dump(agent.reward_net.memory)
+            f.close()
+
+            pd.DataFrame(np.asarray([agent.bid_memory])).to_csv(sys.path.append(os.getcwd() + '/models/bid_history_{}.csv'.format(epoch)), header=None)
+            agent.bid_memory=[]
+            pd.DataFrame(np.asarray([agent.step_memory])).to_csv(sys.path.append(os.getcwd() + '/models/step_history_{}.csv'.format(epoch)), header=None)
+            agent.step_memory=[]
+
 
     print("Total Impressions won {} value = {}".format(agent.total_wins, agent.total_rewards))
     env.close()
